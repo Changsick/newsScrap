7140351cd5f94dd6f1c0ac91a233052d954c26a8
tlakffja <tlakffja3@gmail.com>
2021-03-14 23:59:54 +0900

    title : cron paser 에졔 추가

--- src/main/java/com/gridone/scraping/service/TestScheduleService.java
+++ src/main/java/com/gridone/scraping/service/TestScheduleService.java
@@ -1,9 +1,11 @@
 package com.gridone.scraping.service;
 
+import java.util.Date;
 import java.util.concurrent.ScheduledFuture;
 
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.support.CronSequenceGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +21,9 @@ public class TestScheduleService {
     
     @Scheduled(cron = "0/1 * * 1/1 * ?")
     public void start() {
-    	
+//    	final String cronExpression = "0 0/1 * 1/1 * ?";
+//    	final CronSequenceGenerator generator = new CronSequenceGenerator(cronExpression);
+//    	final Date nextExecutionDate = generator.next(new Date());
+//    	System.out.println(nextExecutionDate);
     }
 }
